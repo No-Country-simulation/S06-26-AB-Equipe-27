18e90d81375ec1d8082b8716429ccb214e6dc8fb
thiagosag <sagihtt@gmail.com>
2026-06-30 16:57:15 -0300

    implementação do mapa de calor/correções

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,9 +8,12 @@ use App\Services\JobService;
 
 class DashboardController extends Controller
 {
-    # Método essencial para percorrer array em View.
-        public function index(){
+    # Método essencial para percorrer array em View e plotar o mapa de calor
+    public function index()
+    {
+        // 1. Mantém a busca original das vagas (Não mexer aqui)
         $jobs = JobPosting::where('company_id', auth()->user()->company->id)->latest()->get();
+        // 3. Inclui a nova variável 'heatPoints' no compact()
         return view('dashboard', compact('jobs'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\JobPostingController;
 use App\Http\Controllers\MatchController;
 use App\Http\Controllers\JobsDashboardController;
 use App\Http\Controllers\SetupController;
+use App\Http\Controllers\MapaController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
@@ -106,7 +107,14 @@ Route::middleware(['auth', 'verified', 'setup.complete'])->group(function () {
     Route::get('/jobs/{id}/edit', [JobPostingController::class, 'edit']);
     Route::put('/jobs/{id}/edit', [JobPostingController::class, 'update']);
     Route::delete('/jobs/{id}/delete', [JobPostingController::class, 'delete']);
+
+    Route::get('/mapa-talentos', function (){
+        return view('mapa');
+    });
+
+    Route::get('/mapa-talentos', [MapaController::class, 'index']);
 });
+
 
 # ====================================
 # Matches
